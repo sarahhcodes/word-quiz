52ebaa3a101ed9f7e209c552aab69c2f1d162508
sarahhcodes <78184189+sarahhcodes@users.noreply.github.com>
2025-08-22 18:32:16 -0600

    setting up project

---
+++ project.py
@@ -0,0 +1,24 @@
+# TO DO:
+# make to do list
+# include text-to-csv utility
+
+import argparse
+import sys
+
+class Player:
+    ...
+
+def main():
+    ...
+
+def make_list():
+    ...
+
+def generate_quiz():
+    ...
+
+def save():
+    ...
+
+if __name__ == "__main__":
+    main()
